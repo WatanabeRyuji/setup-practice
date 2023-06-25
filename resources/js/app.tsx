262149ef2654761539b6aca42bb3c14104a940c6
ryuji <watanaberyuji@arsaga.jp>
2023-06-25 18:30:19 +0900

    :wrench: setup client side Inertia.js react and typescript

---
+++ resources/js/app.tsx
@@ -0,0 +1,15 @@
+import './bootstrap';
+import '../css/app.css'
+import { createInertiaApp } from '@inertiajs/inertia-react';
+import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers'
+import { createRoot } from 'react-dom/client'
+
+const appName = window.document.getElementsByTagName('title')[0]?.innerText || 'Laravel'
+
+createInertiaApp({
+    title: (title) => `${title} - ${appName}`,
+    resolve: (name) => resolvePageComponent(`./pages/${name}.tsx`, import.meta.glob('./pages/**/*.tsx')),
+    setup({el, App, props}) {
+        return createRoot(el).render(<App {...props} />)
+    }
+})
