bc9181e3973387afaa23ed8713a022a9b068cfb9
ryuji <watanaberyuji@arsaga.jp>
2023-06-25 18:33:57 +0900

    :+1: Add InertiaProgress.init

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -3,6 +3,7 @@ import '../css/app.css'
 import { createInertiaApp } from '@inertiajs/inertia-react';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers'
 import { createRoot } from 'react-dom/client'
+import { InertiaProgress } from '@inertiajs/progress'
 
 const appName = window.document.getElementsByTagName('title')[0]?.innerText || 'Laravel'
 
@@ -13,3 +14,5 @@ createInertiaApp({
         return createRoot(el).render(<App {...props} />)
     }
 })
+
+InertiaProgress.init({ color: '#4B5563' })
