5fac848dca7572b5be524f2c4cf364e9e1995bbd
ryuji <watanaberyuji@arsaga.jp>
2023-07-08 23:01:27 +0900

    :sparkels: Add: Base View Model

---
+++ app/ViewModel/ViewModel.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\ViewModel;
+
+use Illuminate\Contracts\Support\Arrayable;
+
+abstract class ViewModel implements Arrayable
+{
+    /**
+     * データをキーバリューのarrayに変換します
+     * @return array
+     */
+    abstract public function toMap(): array;
+
+    /**
+     * 再帰的にarrayに変換します
+     * @return array
+     */
+    final public function toArray(): array
+    {
+        $result = $this->toMap();
+        foreach ($result as $key => $value) {
+            if ($value instanceof Arrayable) {
+                $result[$key] = $value->toArray();
+            } elseif (is_array($value)) {
+                $result[$key] = collect($value)->toArray();
+            } else {
+                $result[$key] = $value;
+            }
+        }
+        return $result;
+    }
+}
