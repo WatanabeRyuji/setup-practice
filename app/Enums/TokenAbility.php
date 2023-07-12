fc69c9c9d416dbb14bbe56d85d06da869031c0ae
ryuji <watanaberyuji@arsaga.jp>
2023-07-12 11:13:52 +0900

    :art: Add: TokenAbilityEnum

---
+++ app/Enums/TokenAbility.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Enums;
+
+use BenSampo\Enum\Enum;
+
+class TokenAbility extends Enum
+{
+    public const AccessApi = 'access-api';
+
+    public const RefreshToken = 'refresh-token';
+}
