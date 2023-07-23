5d3c4899789f47da8118b9ae0d59084ecca0b3ce
ryuji <watanaberyuji@arsaga.jp>
2023-07-23 19:11:25 +0900

    :fire: Remove: rememver token

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
@@ -22,7 +24,6 @@ class UserFactory extends Factory
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-            'remember_token' => Str::random(10),
         ];
     }
 
